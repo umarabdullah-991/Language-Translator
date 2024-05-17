272d8873fb178304e17b8fe7a72fe1ccbad90ec6
Umar <umarabdullah.work@gmail.com>
2024-05-17 15:49:05 +0500

    Functions for buttons added

--- app.js
+++ app.js
@@ -42,3 +42,25 @@ translateBtn.addEventListener("click", () => {
 		toText.setAttribute("placeholder", "Translation");
 	});
 });
+icons.forEach(icon => {
+	icon.addEventListener("click", ({ target }) => {
+		if (!fromText.value || !toText.value) return;
+		if (target.classList.contains("fa-copy")) {
+			if (target.id == "from") {
+				navigator.clipboard.writeText(fromText.value);
+			} else {
+				navigator.clipboard.writeText(toText.value);
+			}
+		} else {
+			let utterance;
+			if (target.id == "from") {
+				utterance = new SpeechSynthesisUtterance(fromText.value);
+				utterance.lang = selectTag[0].value;
+			} else {
+				utterance = new SpeechSynthesisUtterance(toText.value);
+				utterance.lang = selectTag[1].value;
+			}
+			speechSynthesis.speak(utterance);
+		}
+	});
+});
