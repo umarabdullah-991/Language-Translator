265c568a82af47994f8e196d246e462ccfb150c5
Umar <umarabdullah.work@gmail.com>
2024-05-17 15:45:55 +0500

    Added api in JS file

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ exchageIcon.addEventListener("click", () => {
 	selectTag[0].value = selectTag[1].value;
 	selectTag[1].value = tempLang;
 });
+fromText.addEventListener("keyup", () => {
+	if (!fromText.value) {
+		toText.value = "";
+	}
+});
+
+translateBtn.addEventListener("click", () => {
+	let text = fromText.value.trim(),
+		translateFrom = selectTag[0].value,
+		translateTo = selectTag[1].value;
+	if (!text) return;
+	toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+	fetch(apiUrl).then(res => res.json()).then(data => {
+		toText.value = data.responseData.translatedText;
+		data.matches.forEach(data => {
+			if (data.id === 0) {
+				toText.value = data.translation;
+			}
+		});
+		toText.setAttribute("placeholder", "Translation");
+	});
+});
