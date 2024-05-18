b6815601185479cc157ba64f65b7c4f69b00ee8c
Umar <umarabdullah.work@gmail.com>
2024-05-18 16:24:57 +0500

    Corrected functions of translating data

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const fromText = document.querySelector(".from-text"),
   toText = document.querySelector(".to-text"),
   exchangeIcon = document.querySelector(".exchange"),
-  selectingTag = document.querySelector("select"),
-  icons = document.querySelector(".row i"),
-  translation = document.querySelector("button");
-selectingTag.forEach((tag, id) => {
+  selectTag = document.querySelectorAll("select"),
+  icons = document.querySelectorAll(".row i"),
+  translateBtn= document.querySelector("button");
+selectTag.forEach((tag, id) => {
   for (let country_code in countries) {
     let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "ur-PK" ? "selected" : "" ;
     let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
     tag.insertAdjacentHTML("beforeend", option);
   }
 });
-exchageIcon.addEventListener("click", () => {
+exchangeIcon.addEventListener("click", () => {
 	let tempText = fromText.value,
-		tempLang = selectingTag[0].value;
+		tempLang = selectTag[0].value;
 	fromText.value = toText.value;
 	toText.value = tempText;
-	selectingTag[0].value = selectingTag[1].value;
-	selectingTag[1].value = tempLang;
+	selectTag[0].value = selectTag[1].value;
+	selectTag[1].value = tempLang;
 });
 fromText.addEventListener("keyup", () => {
 	if (!fromText.value) {
@@ -25,10 +25,10 @@ fromText.addEventListener("keyup", () => {
 	}
 });
 
-translation.addEventListener("click", () => {
+translateBtn.addEventListener("click", () => {
 	let text = fromText.value.trim(),
-		translateFrom = selectingTag[0].value,
-		translateTo = selectingTag[1].value;
+		translateFrom = selectTag[0].value,
+		translateTo = selectTag[1].value;
 	if (!text) return;
 	toText.setAttribute("placeholder", "Translating...");
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
@@ -55,10 +55,10 @@ icons.forEach(icon => {
 			let utterance;
 			if (target.id == "from") {
 				utterance = new SpeechSynthesisUtterance(fromText.value);
-				utterance.lang = selectingTag[0].value;
+				utterance.lang = selectTag[0].value;
 			} else {
 				utterance = new SpeechSynthesisUtterance(toText.value);
-				utterance.lang = selectingTag[1].value;
+				utterance.lang = selectTag[1].value;
 			}
 			speechSynthesis.speak(utterance);
 		}
